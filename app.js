53b3e70bcffaee60f11b1d03adcbac0ee7350199
Brennan Reed <brtreed3@gmail.com>
2023-01-08 21:36:52 -0500

    Fixed minor errors
    Fixed minor errors and typos

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const { travel } = require('./app_server/controllers/travel');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.user('/travel', travelRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
