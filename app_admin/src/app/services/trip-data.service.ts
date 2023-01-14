2a5ac0e21d08c994476dc09698bd1e6c9c3fdee7
Brennan Reed <brtreed3@gmail.com>
2023-01-14 01:06:35 -0500

    Create Angular Admin site, refactor trip logic, add front and backend logic for adding new trip

---
+++ app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';
+
+import { Trip } from '../models/trip';
+
+@Injectable()
+export class TripDataService {
+
+    // constructor(private http: HttpClient) { }
+    constructor(private http: Http) { }
+
+    private apiBaseUrl = 'http://localhost:3000/api/';
+    private tripUrl = `${this.apiBaseUrl}trips/`;
+
+    public addTrip(formData: Trip): Promise<Trip> {
+        console.log('Inside TripDataService#addTrip');
+        return this.http
+            .post(this.tripUrl, formData)   // pass form data in request body
+            .toPromise()
+            .then(response => response.json() as Trip[])
+            .catch(this.handleError);
+    }
+
+    public getTrips(): Promise<Trip[]> {
+        console.log('Inside TripDataService#getTrips');
+        return this.http
+            .get(`${this.apiBaseUrl}trips`)
+            .toPromise()
+            .then(response => response.json() as Trip[])    // take this out if using httpclient
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('Something has gone wrong', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+}
