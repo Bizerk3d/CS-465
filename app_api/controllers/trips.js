066f85b8372c9a6ebc649d3d14a1976eff09e9f4
Brennan Reed <brtreed3@gmail.com>
2023-02-16 10:33:49 -0500

    More fixes

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose'); //.set('debug', true);
-const Model = mongoose.model('trips');
+const Trip = mongoose.model('trips');
+const User = mongoose.model('users');
 
 // GET: /trips - lists all the trips
 const tripsList = async (req, res) => {
-    Model
+    Trip
         .find({}) // empty filter for all
         .exec((err, trips) => {
             if (!trips) {
@@ -24,7 +25,7 @@ const tripsList = async (req, res) => {
 
 // GET: /trips/:tripCode - returns a single trip
 const tripsFindByCode = async (req, res) => {
-    Model
+    Trip
         .find({ 'code': req.params.tripCode })
         .exec((err, trip) => {
             if (!trip) {
@@ -43,34 +44,64 @@ const tripsFindByCode = async (req, res) => {
         });
 };
 
+const getUser = (req, res, callback) => {
+    if (req.payload && req.payload.email) {
+        User
+            .findOne({ email: req.payload.email })
+            .exec((err, user) => {
+                if (!user) {
+                    return res
+                        .status(404)
+                        .json({ "message": "User not found" });
+                } else if (err) {
+                    console.log(err);
+                    return res
+                        .status(404)
+                        .json(err);
+                }
+                callback(req, res, user.name);
+            });
+    } else {
+        return res
+            .status(404)
+            .json({ "message": "User not found" });
+    }
+}
+
 const tripsAddTrip = async (req, res) => {
-    Model
-        .create({
-            code: req.body.code,
-            name: req.body.name,
-            length: req.body.length,
-            start: req.body.start,
-            resort: req.body.resort,
-            perPerson: req.body.perPerson,
-            image: req.body.image,
-            description: req.body.description
-        },
-        (err, trip) => {
-            if (err) {
-                return res
-                    .status(400)    // bad request, invalid content
-                    .json(err);
-            } else {
-                return res
-                    .status(201)    // created
-                    .json(trip);
-            }
-        });
+    getUser(req, res,
+        (req, res) => {
+        Trip
+            .create({
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            },
+            (err, trip) => {
+                if (err) {
+                    return res
+                        .status(400)    // bad request, invalid content
+                        .json(err);
+                } else {
+                    return res
+                        .status(201)    // created
+                        .json(trip);
+                }
+            });
+        }
+    )
 }
 
 const tripsUpdateTrip = async (req, res) => {
     console.log(req.body);
-    Model
+    getUser(req, res,
+        (req, res) => {
+    Trip
         .findOneAndUpdate({ 'code': req.params.tripCode }, {
             code: req.body.code,
             name: req.body.name,
@@ -102,10 +133,12 @@ const tripsUpdateTrip = async (req, res) => {
                 .status(500)
                 .json(err);
         });
+        }
+    )
 }
 
 const tripsDeleteTrip = async (req, res) => {
-    Model
+    Trip
         .findOneAndDelete({ 'code': req.params.tripCode })
         .exec((err, trip) => {
             if (!trip) {
@@ -122,12 +155,13 @@ const tripsDeleteTrip = async (req, res) => {
                     .json(trip);
             }
         });
-};
+}
 
 module.exports = {
     tripsList,
     tripsFindByCode,
     tripsAddTrip,
     tripsUpdateTrip,
-    tripsDeleteTrip
+    tripsDeleteTrip,
+    getUser
 };
